a20df533f537fc932c13619e526ca7f052bcf8bf
Kevin <104761532+Hasakev@users.noreply.github.com>
2022-11-23 11:23:38 +1000

    Converted snail from surface to "sprite"
    Snail now has rectangle properties, good for future use.
    
    Program: Currently can't run program, don't know why...

--- game.py
+++ game.py
@@ -15,7 +15,6 @@ screenDimensions = (800, 400)
 skyDimensions = (800, 270)
 groundDimensions = (800, 130)
 
-snailPosition = [600, 240]
 snailImages = ["graphics/snail/snail1.png", "graphics/snail/snail2.png"]
 
 #initialise display surface
@@ -28,10 +27,14 @@ font = pygame.font.Font("font/Pixeltype.ttf", 50)
 
 sky_surface = pygame.image.load("graphics/skies.png")
 sky_surface = pygame.transform.scale(sky_surface, skyDimensions).convert_alpha()
+
 ground_surface = pygame.image.load("graphics/ground.png")
 ground_surface = pygame.transform.scale(ground_surface, groundDimensions).convert_alpha()
+
 text_surface = font.render("Brice and Isaac Relationship", False, "black")
-snail_surface = pygame.image.load(snailImages[1])
+
+snail_surface = pygame.image.load(snailImages[0]).convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom = (300, 270))
 
 player_surface = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
 player_rect = player_surface.get_rect(midbottom = (80,270))
@@ -46,15 +49,12 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 270))
     screen.blit(text_surface, (150, 50))
-    snail_surface = pygame.image.load(snailImages[0])
-    screen.blit(snail_surface, (snailPosition[0], snailPosition[1]))
+
+    screen.blit(snail_surface, snail_rect)
     player_rect.left += 1
     screen.blit(player_surface, player_rect)
-    snailPosition[0] -= 1
-    if snailPosition[0] < -50:
-        snailPosition[0] = 800
-    
-    
+
+    snail_rect.left += 1
     
     pygame.display.update()
 
